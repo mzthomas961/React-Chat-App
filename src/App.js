86512e5c62194ea9f7883251c3fdf4651034b15b
Matthew Thomas <mzthomas96@gmail.com>
2022-05-21 22:37:51 -0400

    added imports, read about firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+import React, { useRef, useState } from 'react';
+import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  //  configure later
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
+
 function App() {
   return (
-   <h1>ONE BIG TEST</h1>
+    <div className = "border">
+         <p> blurh</p>
+    </div>
   );
 }
 
